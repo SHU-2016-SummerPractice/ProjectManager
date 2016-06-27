8b2957e6dde9c7056be39a74d0aafbe41943f5d0
Kanon <vipkia@sina.cn>
2016-06-27 11:30:16 +0800

    改进添加project页面

--- ProjectManager/Content/js_file/projectJS.js
+++ ProjectManager/Content/js_file/projectJS.js
@@ -1,1 +1,25 @@
-﻿
+﻿$(document).ready(function () {
+    //点击addProject按钮触发
+    $("#open-add-project").on("click", function () {
+        $("#add-project-message").css({ "display": "none" });
+        $("#add-pb-message").css({ "display": "block" });
+        $("#next-footer").css({ "display": "block" });
+        $("#submit-project").css({ "display": "none" });
+    });
+    //点击下一步触发
+    $("#next-project").click(function () {
+        $("#add-project-message").css({ "display": "block" });
+        $("#add-pb-message").css({ "display": "none" });
+        $("#next-footer").css({ "display": "none" });
+        $("#submit-project").css({ "display": "block" });
+    });
+    //点击返回触发
+    $("back-project").click(function () {
+        $("#add-project-message").css({ "display": "none" });
+        $("#add-pb-message").css({ "display": "block" });
+        $("#next-footer").css({ "display": "block" });
+        $("#submit-project").css({ "display": "none" });
+    });
+
+
+});
