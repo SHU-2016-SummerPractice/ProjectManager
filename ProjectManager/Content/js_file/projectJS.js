d337967730f117c02d2f0c7821279dcbeb60235f
Kanon <vipkia@sina.cn>
2016-06-27 16:33:49 +0800

    优化

--- ProjectManager/Content/js_file/projectJS.js
+++ ProjectManager/Content/js_file/projectJS.js
@@ -3,6 +3,7 @@
     var j = 0;
     //点击addProject按钮触发
     $("#open-add-project").on("click", function () {
+        refresh();
         $("#add-project-message").css({ "display": "none" });
         $("#add-pb-message").css({ "display": "block" });
         $("#next-footer").css({ "display": "block" });
@@ -23,24 +24,18 @@
         $("#submit-project").css({ "display": "none" });
     });
 
-    //添加pb
+    //添加pb,删除单元行
     $("#add-pb").on("click", function () {
         var getId = $(this).parent().prev().attr("id");
-        if (typeof(getId) != "undefined") {
-            //if (j == 0) {
-            //    $("#div-pb-NO").after("<div class='form-group' id='div-pb-NO1'><label for='pb-NO1' class='control-label col-sm-3'>PBNO:</label><input type='text' class='form-control col-sm-5' id='pb-NO1' /><i class='fa fa-lg fa-trash delete-parent-div' aria-hidden='true' style='cursor:pointer;position:relative;left:10px;top:4px;'></i></div>");
-            //    j++;
-            //    i = j;
-            //} else {
-                j++;
-                $("#" + getId).after("<div class='form-group' id='div-pb-NO" + j + "'><label for='pb-NO" + j + "' class='control-label col-sm-3'>PBNO:</label><input type='text' class='form-control col-sm-5' id='pb-NO" + j + "' /><i class='fa fa-lg fa-trash delete-parent-div' aria-hidden='true' style='cursor:pointer;position:relative;left:10px;top:4px;'></i></div>");
-                i = j;
-            //}
+        if (typeof (getId) != "undefined") {
+            j++;
+            $("#" + getId).after("<div class='form-group' id='div-pb-NO" + j + "'><label for='pb-NO" + j + "' class='control-label col-sm-3'>PBNO:</label><input type='text' class='form-control col-sm-5' id='pb-NO" + j + "' /><i class='fa fa-lg fa-trash delete-parent-div' aria-hidden='true' style='cursor:pointer;position:relative;left:10px;top:4px;'></i></div>");
+            i = j;
         } else {
             j = 0;
             $(this).before("<div class='form-group' id='div-pb-NO'><label for='pb-NO' class='control-label col-sm-3'>PBNO:</label><input type='text' class='form-control col-sm-5' id='pb-NO' /><i class='fa fa-lg fa-trash delete-parent-div' aria-hidden='true' style='cursor: pointer;position:relative;left:10px;top:4px;'></i></div>")
         }
-       
+
         var k = 0;
         $(".delete-parent-div").on("click", function () {
             $(this).parent().remove();
@@ -50,7 +45,4 @@
             k++;
         });
     });
-
-    //删除单元行
-   
 });
