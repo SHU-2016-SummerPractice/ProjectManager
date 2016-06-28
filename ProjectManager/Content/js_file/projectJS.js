1610487f76c4f93c3b32abf375a3c74f62985663
Kanon <vipkia@sina.cn>
2016-06-28 11:42:09 +0800

    更改scroll样式

--- ProjectManager/Content/js_file/projectJS.js
+++ ProjectManager/Content/js_file/projectJS.js
@@ -3,7 +3,6 @@
     var j = 0;
     //点击addProject按钮触发
     $("#open-add-project").on("click", function () {
-        refresh();
         $("#add-project-message").css({ "display": "none" });
         $("#add-pb-message").css({ "display": "block" });
         $("#next-footer").css({ "display": "block" });
@@ -44,5 +43,15 @@
             }
             k++;
         });
+    });
+
+    $("#show-project").niceScroll({
+        cursorcolor: "#ccc",//#CC0071 光标颜色 
+        cursoropacitymax: 1, //改变不透明度非常光标处于活动状态（scrollabar“可见”状态），范围从1到0 
+        touchbehavior: false, //使光标拖动滚动像在台式电脑触摸设备 
+        cursorwidth: "5px", //像素光标的宽度 
+        cursorborder: "0", //     游标边框css定义 
+        cursorborderradius: "5px",//以像素为光标边界半径 
+        autohidemode: false //是否隐藏滚动条 
     });
 });
