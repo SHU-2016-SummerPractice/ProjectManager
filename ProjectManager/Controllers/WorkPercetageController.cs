7e5c3f5552653c36b9ed5ae5eb9413133377c279
Jason Song <wolfogre@outlook.com>
2016-07-03 16:34:22 +0800

    WorkPercetage的进度条和更新完成

--- ProjectManager/Controllers/WorkPercetageController.cs
+++ ProjectManager/Controllers/WorkPercetageController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.WebPages;
 using ProjectManager.Models;
 
 namespace ProjectManager.Controllers
@@ -17,5 +18,27 @@ namespace ProjectManager.Controllers
             ViewBag.workPercetages = workPercetages;
             return View();
         }
-    }
+
+	    public ActionResult Update(string staffid, string percentage, string project, string memo)
+	    {
+			if(staffid == null || staffid.IsEmpty()
+				|| percentage == null || percentage.IsEmpty()
+				|| project == null || project.IsEmpty()
+				|| memo == null || memo.IsEmpty()
+				)
+				return null;
+			ModelDbContext mdb = new ModelDbContext();
+			IQueryable<Domain> workPercetages = mdb.Domains;
+		    int staff = int.Parse(staffid);
+			Domain aim = (from workPercetage in workPercetages
+			    where workPercetage.StaffId == staff
+						  select workPercetage).First();
+		    aim.Percentage = 100 > int.Parse(percentage) ? int.Parse(percentage) : 100;
+			aim.Project = project;
+			aim.Memo = memo;
+		    mdb.SaveChanges();
+			return null;
+		}
+
+	}
 }
